2da8575812e3416e01260a6e4e95e4850a31e5bc
Makketeller <Makketeller@users.noreply.github.com>
2025-07-13 09:46:15 +0000

    deploy: 0aaf5b2873bee45d1b2e0ec2e658fd70d316c837

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
 console.log("Own webiste script loaded successfully!");
+
+document.addEventListener('DOMContentLoaded', function () {
+    const projectsLink = document.getElementById('nav-projects');
+    const solutionsLink = document.getElementById('nav-solutions');
+    const aboutMeLink = document.getElementById('nav-about-me');
+
+    // When "Solutions" is clicked
+    solutionsLink.addEventListener('click', function () {
+        projectsLink.classList.remove('active');
+        projectsLink.removeAttribute('aria-current');
+        aboutMeLink.classList.remove('active');
+        aboutMeLink.removeAttribute('aria-current');
+
+        solutionsLink.classList.add('active');
+        solutionsLink.setAttribute('aria-current', 'page');
+    });
+
+    // When "Projects" is clicked
+    projectsLink.addEventListener('click', function () {
+        solutionsLink.classList.remove('active');
+        solutionsLink.removeAttribute('aria-current');
+        aboutMeLink.classList.remove('active');
+        aboutMeLink.removeAttribute('aria-current');
+
+        projectsLink.classList.add('active');
+        projectsLink.setAttribute('aria-current', 'page');
+    });
+
+     // When "About Me" is clicked
+    aboutMeLink.addEventListener('click', function () {
+        solutionsLink.classList.remove('active');
+        solutionsLink.removeAttribute('aria-current');
+        projectsLink.classList.remove('active');
+        projectsLink.removeAttribute('aria-current');
+
+        aboutMeLink.classList.add('active');
+        aboutMeLink.setAttribute('aria-current', 'page');
+    });
+});
